ccb00ec12b9753712b38557a956cab477c57a7f9
Morandev <moranfcnddev@gmail.com>
2022-09-17 03:12:21 -0300

    add: defining interface FilmQueryService

---
+++ src/main/java/com/morandev/movieadvisor/service/FilmQueryService.java
@@ -0,0 +1,16 @@
+package com.morandev.movieadvisor.service;
+
+import java.util.Collection;
+
+import com.morandev.movieadvisor.Film;
+
+public interface FilmQueryService {
+
+	Collection<Film> exec();
+	FilmQueryService anyGenre(String... genres);
+	FilmQueryService allGenres(String... genres);
+	FilmQueryService year(String year);
+	FilmQueryService betweenYears(String from, String to);
+	FilmQueryService titleCOntains(String title);
+
+}
