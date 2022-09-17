496affef7c817bab29290997d7f0acb21006b63b
Morandev <moranfcnddev@gmail.com>
2022-09-17 02:49:14 -0300

    refactor: AppConfig class now is injected into FilmDaoImplMemory repository class

--- src/main/java/com/morandev/movieadvisor/config/AppConfig.java
+++ src/main/java/com/morandev/movieadvisor/config/AppConfig.java
@@ -1,11 +1,37 @@
 package com.morandev.movieadvisor.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 /* escanea todos los bean del packete movieadvisor y los mas abajo */
 @ComponentScan(basePackages = "com.morandev.movieadvisor")
+/* carga nuestro archivo de propiedades */
+@PropertySource("classpath:/app.properties")
 public class AppConfig {
-
+	
+	/* la idea es inyectar AppConfig en las clases que necesiten
+	 * valores del archivo de propiedades */
+	
+	/* con la etiqueta value leemos las propiedades*/
+	@Value("${file.path}")
+	private String filePath;
+	@Value("${file.csv.separator}")
+	private String lineSeparator;
+	@Value("${file.csv.list_separator}")
+	private String listItemSeparator;
+	
+	public String getFilePath() {
+		return filePath;
+	}
+	
+	public String getLineSeparator() {
+		return lineSeparator;
+	}
+	
+	public String getListItemSeparator() {
+		return listItemSeparator;
+	}
 }
--- src/main/java/com/morandev/movieadvisor/dao/FilmDaoImplMemory.java
+++ src/main/java/com/morandev/movieadvisor/dao/FilmDaoImplMemory.java
@@ -4,19 +4,27 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.morandev.movieadvisor.Film;
+import com.morandev.movieadvisor.config.AppConfig;
 
 /* bean repository */
 @Repository
 public class FilmDaoImplMemory implements FilmDao {
 	
+	/* Inyectamos la dependencia de AppConfig*/
+	@Autowired
+	private AppConfig appConfig;
+	
 	private List<Film> movies = new ArrayList<>();
 	
 	
 	public void init() throws Exception {
-		movies = UtilFilmFileReader.readFile(null, null, null);
+		movies = UtilFilmFileReader.readFile( appConfig.getFilePath(),
+											appConfig.getLineSeparator(),
+											appConfig.getListItemSeparator());
 	}
 	
 	@Override
