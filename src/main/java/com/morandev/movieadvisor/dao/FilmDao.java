b79467b76f627f1d25da3e250f4f399eb967a5e5
Morandev <moranfcnddev@gmail.com>
2022-09-17 02:07:20 -0300

    add: DAO or repository film interface

---
+++ src/main/java/com/morandev/movieadvisor/dao/FilmDao.java
@@ -0,0 +1,13 @@
+package com.morandev.movieadvisor.dao;
+
+import java.util.Collection;
+
+import com.morandev.movieadvisor.Film;
+
+public interface FilmDao {
+	Film findById(long id);
+	Collection<Film> findAll();
+	void insert(Film film);
+	void edit(Film film);
+	void delete(long id);
+}
